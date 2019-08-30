b834e479e8d941c7288e07c466759e2721867341
dingxin <zzuxind@163.com>
2019-08-30 19:27:34 +0800

    建表sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,14 @@
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(64) NOT NULL,
-  age   int(4) NOT NULL
-);
+CREATE TABLE `parking_boy` (
+`parkingboy_id`  varchar(36) NOT NULL ,
+PRIMARY KEY (`parkingboy_id`)
+)
+;
+
+CREATE TABLE `parking_lot` (
+`parkinglot_id`  varchar(36) NOT NULL ,
+`capacity`  int NULL ,
+`parkingboy_id`  varchar(36) NULL ,
+PRIMARY KEY (`parkinglot_id`),
+FOREIGN KEY (`parkingboy_id`) REFERENCES `parking_boy` (`parkingboy_id`)
+)
+;
